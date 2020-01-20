cc933a38c749ba865e1b1073fe31e544c6ccf20b
Emacs Hacker <emacshacker@example.com>
2020-01-19 19:38:46 -0500

    Add ability to toggle reference syntax

--- emacs-transclusion.el
+++ emacs-transclusion.el
@@ -1,6 +1,20 @@
 (setq first-marker (make-marker))
 (setq second-marker (make-marker))
-      
+
+(setq emacs-transclusion/overlays '())
+
+(setq emacs-transclusion/refernece-display-state nil)
+
+(defun emacs-transclusion/toggle-display-embed-syntax ()
+
+  "Toggles displaying of embed syntax in documents."
+
+  ;;Inverse turthyness of reference display state
+  (setq emacs-transclusion/refernece-display-state (not emacs-transclusion/refernece-display-state))
+  (dolist (cur-overlay emacs-transclusion/overlays)
+    (overlay-put cur-overlay 'invisible emacs-transclusion/refernece-display-state)))
+
+
 (defun emacs-transclusion/get-file-contents (path)
   "Returns content of file as string"
   (with-temp-buffer
@@ -8,9 +22,14 @@
     (buffer-string)))
 
 (defun emacs-transclusion/find-embed-syntax-in-current-buffer ()
-    (let ((buffer-point (re-search-forward "\\[EMBED: \\([^\\]*\\)\\(]\\)")))
+  (let ((buffer-point (re-search-forward "\\[EMBED: \\([^\\]*\\)\\(]\\)"))
+	(overlay-ref nil))
       (when (match-string 0)
-	  
+
+
+	(setq overlay-ref (make-overlay (match-beginning 0) (match-end 0)))
+	(push overlay-ref emacs-transclusion/overlays)
+
 	;re-search-forward moves point to end of match.
 	;save position
 	(setq first-marker (point))
@@ -33,5 +52,4 @@
     (emacs-transclusion/find-embed-syntax-in-current-buffer)
     (delete-region first-marker second-marker)
 )
-
 
